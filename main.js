59ab73305f5ca1dae6dc1b57161c2824033be582
NavjotG <n.ghotra1@share.epsb.ca>
2022-11-23 11:09:38 -0700

    dones

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-// JavaScript Code
+// User Login
+
+document.getElementById("button").addEventListener("click", PasswordL);
+
+function PasswordL() {
+  // Input
+  let passwords = document.getElementById("password").value;
+  let usernames = document.getElementById("username").value;
+
+  //   Process
+  if (usernames === "admin" && passwords === "1234") {
+    alert("Login Successful");
+  } else if (usernames !== "admin" && passwords === "1234") {
+    alert("Username Incorrect");
+  } else if (usernames === "admin" && passwords !== "1234") {
+    alert("Invalid Password");
+  } else {
+    alert("Input Something");
+  }
+}
